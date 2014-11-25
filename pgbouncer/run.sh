d89b00d601118342e382a072a106458aa1bfecb7
Givi Khojanashvili <gigovich@gmail.com>
2014-11-25 16:19:35 +0100

    Add Pgbouncer

--- pgbouncer/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/env sh
-
-# Prepare template by envirement variables
-/usr/bin/python /home/docker/template.py
-
-# Run pgbouncer with generated config
-/usr/sbin/pgbouncer /etc/pgbouncer/pgbouncer.ini
